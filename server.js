b1c56d51b5fb8897bb246cf1fd5e880a1f38e2ef
piyushnakade <nakade.piyush@gmail.com>
2016-10-17 21:04:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
 });
-
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter. tostring());
+});
 app.get('ui/article one.html', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'article one.html*'));
     
