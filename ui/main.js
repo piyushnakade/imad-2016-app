5e357b8f34acb46cd1bf48d74809b0889159438a
piyushnakade <nakade.piyush@gmail.com>
2016-10-17 22:46:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,27 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function (){
     // make request to counter endpoint
-     //capture response and store it in variable 
-     //render the variable in correct span
-     counter = counter + 1;
-     var span = document.getElementById('count');
+    var request = new XMLHttpRequest();
+     //capture response and store it in variable
+     request.onreadystatechange = function (){
+         if(request.readystate===XMLHttpRequest.DONE)
+         {
+             //take some action
+             if (status==200){
+                 var conuter = request.responseText;
+                 var span = document.getElementById('count');
      span.innerHTML = counter.toString();
      
+             }
+             
+     }
+     
+     
+    //not done yet
+     };
+     //make the request
+     request.open('http://piyushnakade.imad.hasura-app.io/counter',true);
+     request.send(null);
       
      
     
